54fe6663b92cb03671498427bf83bf2f70528404
“Yuu” <yuu190825@icloud.com>
2022-05-31 18:34:15 +0800

    design commit

--- Calise/ContentView.swift
+++ Calise/ContentView.swift
@@ -9,8 +9,108 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.gray.ignoresSafeArea()
+            VStack {
+                HStack {
+                    Text("0 ")
+                        .frame(width: 300, height: 50)
+                        .background(.white)
+                    Button(action: {}) {
+                        Text("<-")
+                            .frame(width: 75, height: 50)
+                            .foregroundColor(.white)
+                            .background(.blue)
+                    }
+                }
+                .frame(width: UIScreen.main.bounds.width, height: 50)
+                HStack {
+                    Text("0 ")
+                        .frame(width: 300, height: 50)
+                        .background(.white)
+                    Button(action: {}) {
+                        Text("<-")
+                            .frame(width: 75, height: 50)
+                            .foregroundColor(.white)
+                            .background(.blue)
+                    }
+                }
+                .frame(width: UIScreen.main.bounds.width, height: 50)
+                HStack {
+                    Text("0 ")
+                        .frame(width: 300, height: 50)
+                        .background(.white)
+                    Button(action: {}) {
+                        Text("<-")
+                            .frame(width: 75, height: 50)
+                            .foregroundColor(.white)
+                            .background(.blue)
+                    }
+                }
+                .frame(width: UIScreen.main.bounds.width, height: 50)
+                HStack {
+                    Text("0 ")
+                        .frame(width: 300, height: 50)
+                        .background(.white)
+                    Button(action: {}) {
+                        Text("<-")
+                            .frame(width: 75, height: 50)
+                            .foregroundColor(.white)
+                            .background(.blue)
+                    }
+                }
+                .frame(width: UIScreen.main.bounds.width, height: 50)
+                HStack {
+                    Text("0 ")
+                        .frame(width: 300, height: 50)
+                        .background(.white)
+                    Button(action: {}) {
+                        Text("<-")
+                            .frame(width: 75, height: 50)
+                            .foregroundColor(.white)
+                            .background(.blue)
+                    }
+                }
+                .frame(width: UIScreen.main.bounds.width, height: 50)
+                HStack {
+                    Text("0 ")
+                        .frame(width: 300, height: 50)
+                        .background(.white)
+                    Button(action: {}) {
+                        Text("<-")
+                            .frame(width: 75, height: 50)
+                            .foregroundColor(.white)
+                            .background(.blue)
+                    }
+                }
+                .frame(width: UIScreen.main.bounds.width, height: 50)
+                HStack {
+                    Text("0 ")
+                        .frame(width: 300, height: 50)
+                        .background(.white)
+                    Button(action: {}) {
+                        Text("<-")
+                            .frame(width: 75, height: 50)
+                            .foregroundColor(.white)
+                            .background(.blue)
+                    }
+                }
+                .frame(width: UIScreen.main.bounds.width, height: 50)
+                HStack {
+                    Text("0 ")
+                        .frame(width: 300, height: 50)
+                        .background(.white)
+                    Button(action: {}) {
+                        Text("<-")
+                            .frame(width: 75, height: 50)
+                            .foregroundColor(.white)
+                            .background(.blue)
+                    }
+                }
+                .frame(width: UIScreen.main.bounds.width, height: 50)
+            }
+            .frame(width: UIScreen.main.bounds.width, height: 500)
+        }
     }
 }
 
