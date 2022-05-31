18bd510a8945b9ff732b25ab753db4c842dd1a38
“Yuu” <yuu190825@icloud.com>
2022-05-31 14:23:25 +0800

    Initial Commit

---
+++ Calise/CaliseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaliseApp.swift
+//  Calise
+//
+//  Created by 王ユウ on R 4/05/31.
+//
+
+import SwiftUI
+
+@main
+struct CaliseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calise/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calise
+//
+//  Created by 王ユウ on R 4/05/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
