96c2e9f3ea771a8072d293c325111f7a9f22cdb3
“Yuu” <yuu190825@icloud.com>
2022-06-01 14:57:35 +0800

    design commit M10601_2

--- Calise/ContentView.swift
+++ Calise/ContentView.swift
@@ -8,46 +8,232 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var oouControl = 1.0
+    @State private var dotControl = 0.0
     var body: some View {
         ZStack {
             Color.gray.ignoresSafeArea()
-            VStack(spacing: 0) {
+            VStack(spacing: 6) {
                 GeometryReader {
                     geometry in
-                    HStack(spacing: 0) {
+                    HStack(spacing: 6) {
                         Text("0 ")
                             .font(.system(size: 24))
-                            .frame(width: ((geometry.size.width / 4) * 3), height: geometry.size.height, alignment: .trailing)
+                            .frame(maxWidth: .infinity, maxHeight: geometry.size.height, alignment: .trailing)
                             .background(.white)
                         Button(action: {}) {
                             Text("<-")
-                                .font(.system(size: 24))
+                                .font(.system(size: 24, weight: .bold))
                         }
-                        .frame(width: (geometry.size.width / 4), height: geometry.size.height)
+                        .frame(width: geometry.size.height, height: geometry.size.height)
                         .foregroundColor(.white)
                         .background(.blue)
                     }
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
                 }
-                HStack(spacing: 0) {
+                HStack(spacing: 6) {
+                    VStack {
+                        Slider(value: $oouControl, in: 0...2)
+                        Text("4/5")
+                            .font(.system(size: 24))
+                            .frame(width: 50, height: 40)
+                            .background(.white)
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    VStack {
+                        Slider(value: $dotControl, in: 0...2)
+                        Text("0")
+                            .font(.system(size: 24))
+                            .frame(width: 40, height: 40)
+                            .background(.white)
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
                 }
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
-                HStack(spacing: 0) {
+                HStack(spacing: 6) {
+                    Button(action: {}) {
+                        Text("C")
+                            .font(.system(size: 24, weight: .bold))
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .foregroundColor(.white)
+                    .background(.blue)
+                    Button(action: {}) {
+                        Text("+/-")
+                            .font(.system(size: 24, weight: .bold))
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .foregroundColor(.white)
+                    .background(.blue)
+                    Button(action: {}) {
+                        Text("sqrt")
+                            .font(.system(size: 24, weight: .bold))
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .foregroundColor(.white)
+                    .background(.blue)
+                    Button(action: {}) {
+                        Text("^")
+                            .font(.system(size: 24, weight: .bold))
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .foregroundColor(.white)
+                    .background(.blue)
                 }
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
-                HStack(spacing: 0) {
+                HStack(spacing: 6) {
+                    Button(action: {}) {
+                        Text("MC")
+                            .font(.system(size: 24, weight: .bold))
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .foregroundColor(.white)
+                    .background(.blue)
+                    Button(action: {}) {
+                        Text("MR")
+                            .font(.system(size: 24, weight: .bold))
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .foregroundColor(.white)
+                    .background(.blue)
+                    Button(action: {}) {
+                        Text("M-")
+                            .font(.system(size: 24, weight: .bold))
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .foregroundColor(.white)
+                    .background(.blue)
+                    Button(action: {}) {
+                        Text("M+")
+                            .font(.system(size: 24, weight: .bold))
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .foregroundColor(.white)
+                    .background(.blue)
                 }
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
-                HStack(spacing: 0) {
+                HStack(spacing: 6) {
+                    Button(action: {}) {
+                        Text("7")
+                            .font(.system(size: 24, weight: .bold))
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .foregroundColor(.white)
+                    .background(.blue)
+                    Button(action: {}) {
+                        Text("8")
+                            .font(.system(size: 24, weight: .bold))
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .foregroundColor(.white)
+                    .background(.blue)
+                    Button(action: {}) {
+                        Text("9")
+                            .font(.system(size: 24, weight: .bold))
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .foregroundColor(.white)
+                    .background(.blue)
+                    Button(action: {}) {
+                        Text("/")
+                            .font(.system(size: 24, weight: .bold))
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .foregroundColor(.white)
+                    .background(.blue)
                 }
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
-                HStack(spacing: 0) {
+                HStack(spacing: 6) {
+                    Button(action: {}) {
+                        Text("4")
+                            .font(.system(size: 24, weight: .bold))
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .foregroundColor(.white)
+                    .background(.blue)
+                    Button(action: {}) {
+                        Text("5")
+                            .font(.system(size: 24, weight: .bold))
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .foregroundColor(.white)
+                    .background(.blue)
+                    Button(action: {}) {
+                        Text("6")
+                            .font(.system(size: 24, weight: .bold))
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .foregroundColor(.white)
+                    .background(.blue)
+                    Button(action: {}) {
+                        Text("*")
+                            .font(.system(size: 24, weight: .bold))
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .foregroundColor(.white)
+                    .background(.blue)
                 }
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
-                HStack(spacing: 0) {
+                HStack(spacing: 6) {
+                    Button(action: {}) {
+                        Text("1")
+                            .font(.system(size: 24, weight: .bold))
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .foregroundColor(.white)
+                    .background(.blue)
+                    Button(action: {}) {
+                        Text("2")
+                            .font(.system(size: 24, weight: .bold))
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .foregroundColor(.white)
+                    .background(.blue)
+                    Button(action: {}) {
+                        Text("3")
+                            .font(.system(size: 24, weight: .bold))
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .foregroundColor(.white)
+                    .background(.blue)
+                    Button(action: {}) {
+                        Text("-")
+                            .font(.system(size: 24, weight: .bold))
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .foregroundColor(.white)
+                    .background(.blue)
                 }
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
-                HStack(spacing: 0) {
+                HStack(spacing: 6) {
+                    Button(action: {}) {
+                        Text("0")
+                            .font(.system(size: 24, weight: .bold))
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .foregroundColor(.white)
+                    .background(.blue)
+                    Button(action: {}) {
+                        Text(".")
+                            .font(.system(size: 24, weight: .bold))
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .foregroundColor(.white)
+                    .background(.blue)
+                    Button(action: {}) {
+                        Text("=")
+                            .font(.system(size: 24, weight: .bold))
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .foregroundColor(.white)
+                    .background(.blue)
+                    Button(action: {}) {
+                        Text("+")
+                            .font(.system(size: 24, weight: .bold))
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .foregroundColor(.white)
+                    .background(.blue)
                 }
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
             }
